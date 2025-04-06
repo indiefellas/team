626768b1e8c5c7d7c08b0f57fb2b4b0078654138
Jb Carreon <jbcarreon212@gmail.com>
2025-04-06 21:58:06 +0800

    fella nekos!

--- src/components/Fellas.svelte
+++ src/components/Fellas.svelte
@@ -5,6 +5,7 @@
 {#each Fellas as fella}
     <a href={fella.links[0].url}>
         <div class="reset link-item fella-item">
+            <img class="fella-img" src="/img/fellas/{fella.name}.svg" alt="" />
             <h2 class="jua reset">{fella.name}</h2>
             <p class="reset">{fella.description}</p>
         </div>
