4b4f9719f505d1985b79c0c6200a6bac8a6177df
Jb Carreon <jbcarreon212@gmail.com>
2025-04-06 14:05:33 +0800

    add stuff into the teams page

---
+++ src/components/Fellas.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Fellas from '../assets/fellas.json'
+</script>
+
+{#each Fellas as fella}
+    <a href={fella.links[0].url}>
+        <div class="reset link-item fella-item">
+            <h2 class="jua reset">{fella.name}</h2>
+            <p class="reset">{fella.description}</p>
+        </div>
+    </a>
+{/each}
